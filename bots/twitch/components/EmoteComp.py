ebe16c70ca5072d15bb0e5b596e9b5f072f2ba8a
dreaded_ <haileymsquad@gmail.com>
2026-04-05 12:50:25 -0500

    Create EmoteComp.py

---
+++ bots/twitch/components/EmoteComp.py
@@ -0,0 +1,11 @@
+import asyncio
+
+from eventapi.EventApi import EventApi
+from eventapi.WebSocket import (
+    EventType,
+    ResponseTypes,
+    SubscriptionCondition,
+    SubscriptionData,
+)
+
+import requests
