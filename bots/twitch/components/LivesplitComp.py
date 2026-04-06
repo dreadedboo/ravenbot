3d35d9445bc9f329e90bd62c33f7c5a88ea68090
dreaded_ <haileymsquad@gmail.com>
2026-04-06 18:00:39 -0500

    cleaned up livesplit integration further, added command to reconnect if connection lost

--- bots/twitch/components/LivesplitComp.py
+++ bots/twitch/components/LivesplitComp.py
@@ -10,32 +10,32 @@ class Livesplit(commands.Component):
 
     def __init__(self, bot) -> None:
         self.bot = bot
-        self.sckt = LivesplitConnection()
+        self.livesplit = LivesplitConnection()
 
     @commands.group(name="livesplit", aliases=["lsplit"])
-    async def livesplit(self, ctx: commands.Context, func: str = None) -> None:
-        if self.sckt.get_string("ping") == "pong":
+    async def lsplit(self, ctx: commands.Context, func: str = None) -> None:
+        if self.livesplit.get_string("ping") == "pong":
             await ctx.send("Currently connected to livesplit. Available commands: !pb !bpt !sob")
         else:
             await ctx.send("Could not connect to livesplit")
             LOGGER.info("Could not connect to livesplit")
 
     @commands.is_moderator()
-    @livesplit.command(name="game")
+    @lsplit.command(name="game")
     async def set_game_from_splits(self, ctx: commands.Context) -> None:
-        game_name = self.sckt.get_string("getgamename")
-        if game_name is not False:
+        game_name = self.livesplit.get_string("getgamename")
+        if game_name != "":
             await change_game(self.bot, ctx, str(game_name))
         else:
             await ctx.send("Failed to receive data from Livesplit")
             LOGGER.error("Failed to receive data from Livesplit")
 
     @commands.is_moderator()
-    @livesplit.command(name="title")
+    @lsplit.command(name="title")
     async def set_title_from_category(self, ctx: commands.Context) -> None:
-        game_name = self.sckt.get_string("getgamename")
-        category = self.sckt.get_string("getcategoryname")
-        if game_name is not False and category is not False:
+        game_name = self.livesplit.get_string("getgamename")
+        category = self.livesplit.get_string("getcategoryname")
+        if game_name != "" and category != "":
             title: str = f"{game_name} - {category}"
             await change_title(self.bot, ctx, title)
         else:
@@ -43,24 +43,45 @@ class Livesplit(commands.Component):
             LOGGER.error("Failed to receive data from Livesplit")
 
     @commands.is_moderator()
-    @livesplit.command(name="setup")
+    @lsplit.command(name="setup")
     async def set_game_and_title(self, ctx: commands.Context):
         await self.set_game_from_splits(ctx)
         await self.set_title_from_category(ctx)
 
+
+    @commands.is_moderator()
+    @lsplit.command(name="connect")
+    async def connect_to_server(self, ctx: commands.Context) -> None:
+        self.livesplit.close()
+        self.livesplit = LivesplitConnection()
+        await self.lsplit(ctx)
+
     @commands.command(name="pb")
     async def get_personal_best(self, ctx: commands.Context) -> None:
-        reply = self.sckt.get_time("getfinaltime Personal Best")
+        reply = self.livesplit.get_string("getfinaltime Personal Best")
+        if reply != "":
+            await ctx.send(f"Splits PB: {reply[:-8]}")
+        else:
+            await ctx.send("Failed to receive data from Livesplit")
+            LOGGER.error("Failed to receive data from Livesplit")
         # this command will also get leaderboard PB from SRC
-        await ctx.send(f"Splits PB: {reply}")
+
 
     @commands.command(name="bpt")
     async def get_bpt(self, ctx: commands.Context) -> None:
-        reply = self.sckt.get_time("getbestpossibletime")
-        await ctx.send(f"Best Possible Time: {reply}")
+        reply = self.livesplit.get_string("getbestpossibletime")
+        if reply != "":
+            await ctx.send(f"Best Possible Time: {reply[:-8]}")
+        else:
+            await ctx.send("Failed to receive data from Livesplit")
+            LOGGER.error("Failed to receive data from Livesplit")
+
 
     @commands.command(name="sob")
     async def get_sob(self, ctx: commands.Context) -> None:
-        reply = self.sckt.get_time("getfinaltime Best Segments")
-        await ctx.send(f"Sum of Best: {reply}")
-
+        reply = self.livesplit.get_string("getfinaltime Best Segments")
+        if reply != "":
+            await ctx.send(f"Sum of Best: {reply[:-8]}")
+        else:
+            await ctx.send("Failed to receive data from Livesplit")
+            LOGGER.error("Failed to receive data from Livesplit")
--- utilities/LivesplitUtils.py
+++ utilities/LivesplitUtils.py
@@ -14,28 +14,21 @@ class LivesplitConnection(socket):
         super().__init__(AF_INET, SOCK_STREAM)
         try:
             self.connect((HOST, PORT))
-        except ConnectionRefusedError:
+            LOGGER.info("Connected to LiveSplit Server")
+        except ConnectionRefusedError or ConnectionAbortedError:
             LOGGER.error("Failed to connect to Livesplit server")
 
-    def get_string(self, command: str) -> bool | str:
+    def get_string(self, command: str) -> str:
+        data = ""
         self.settimeout(5.0)
         try:
             self.sendall(f"{command}\r\n".encode('utf-8'))
-            data: str = self.recv(1024).decode('UTF-8')
+            try:
+                data: str = self.recv(1024).decode('UTF-8')
+                LOGGER.info(f"Command: {command} ran successfully. Received: {data[:-1]}")
+                return data[:-1]
+            except ConnectionAbortedError:
+                LOGGER.error("Lost connection to LiveSplit")
         except TimeoutError:
             LOGGER.error("Livesplit server connection timed out")
-            return False
-        LOGGER.info(f"Command: {command} ran successfully. Received: {data[:-1]}")
-        return data[:-1]
-
-
-    def get_time(self, command: str) -> bool | str:
-        self.settimeout(5.0)
-        try:
-            self.sendall(f"{command}\r\n".encode('utf-8'))
-            data: str = self.recv(1024).decode('UTF-8')
-        except TimeoutError:
-            LOGGER.error("Livesplit server connection timed out")
-            return False
-        LOGGER.info(f"Command: {command} ran successfully. Received: {data[:-9]}")
-        return data[:-9]
+        return data
